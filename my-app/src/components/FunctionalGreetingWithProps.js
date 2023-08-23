739f4eebcaf2f94502ac1057d7b8a41bb24d5150
jatoad <jasperhmca.todd@gmail.com>
2023-08-23 06:52:04 +0000

    added notes on props and css added props to FunctionalGreetingWithProps

--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-const FunctionalGreetingWithProps = () =>  <h1>works</h1>;
+const FunctionalGreetingWithProps = (props) =>  {
+    return <h1>Hello, {props.name}! I see youre {props.age}. {props.greeeting} </h1>;
+}
 
 
 export default FunctionalGreetingWithProps;
