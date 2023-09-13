b7bd04c9129befbc057c8d84e8c56517b02c3d96
jatoad <jasperhmca.todd@gmail.com>
2023-09-13 20:42:32 +0000

    added notes and examples on hooks (useEffect & useState)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,21 +8,25 @@ import './App.css';
 // import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 // import NestingComponents from './components/nested-components/NestingComponents';
 // import MethodAsPropsParent from './components/MethodAsPropsParent';
-// import RenderingLists from './components/rendering-lists /RenderingLists';
+// import RenderingLists from './components/RenderingLists';
 // import Clocks from './components/Clocks'
 // import LifeCyclesCDM from './components/LifeCyclesCDM';
 // import LifecyclesCDU from './components/LifecyclesCDU';
 // import LifeCyclesCWU from './components/LifeCyclesCWU';
 // import ControlledForm from './components/ControlledForm';
 // import UncontrolledForm from './components/UncontrolledForm';
-// import SearchBar from '../../components/forms/SearchBar';
-import HookCounter from './components/HookCounter';
-
+// import SearchBar from '../components/SearchBar';
+// import HookCounter from './components/HookCounter';
+// import ControlledFormHooks from './components/ControlledFormHooks';
+// import UseStateWithArrays from './components/UseStateWithArrays';
+// import UseStateWithObjects from './components/UseStateWithObjects';
+// import UseEffectCounter from './components/UseEffectCounter';
+import UseEffectCounterContainer from './components/UseEffectCounterContainer';
 
 function App() {
   return (
     <div className='App'>
-      <HookCounter />
+      <UseEffectCounterContainer />
     </div>
   )
 }
